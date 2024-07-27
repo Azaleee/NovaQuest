6ebc4f2c45170e457aef5fe2ee4ea5dad9788d95
Macbook M1 pro <macbookm1pro@canette.local>
2024-07-27 17:14:43 +0200

    add others files

--- NovaQuest.cs
+++ NovaQuest.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace NovaQuest
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class NovaQuest : Mod
+	public class NovaQuest : ModSystem
 	{
+		private UserInterface userInterface;
+		private UITest test;
+		public override void Load()
+		{
+			if (!Main.dedServ)
+			{
+				test = new UITest();
+				test.Activate();
+				userInterface = new UserInterface();
+				userInterface.SetState(test);
+			}
+		}
+		public override void UpdateUI(GameTime gameTime)
+		{
+			if (userInterface != null)
+				userInterface.Update(gameTime);
+		}
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			int mouseTextIndex;
 
+			mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+			if (mouseTextIndex != 1)
+			{
+				layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+					"Test lol",
+					delegate
+					{
+						if (userInterface != null)
+							userInterface.Draw(Main.spriteBatch, new GameTime());
+						return true;
+					},
+					InterfaceScaleType.UI)
+				);
+			}
+		}
 	}
 }
--- obj/Debug/net8.0/NovaQuest.AssemblyInfo.cs
+++ obj/Debug/net8.0/NovaQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovaQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540f4fdc7129147e9238ccd6587c451834b7a8fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovaQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovaQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
